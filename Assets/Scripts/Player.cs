613f5e5f1550cd72e4075e0f7cdde835c3fc1ff8
Gabrielius <gdobrovolskis97@gmail.com>
2021-02-05 17:10:26 +0000

    improvements and comments

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 using Mirror;
 public class Player : NetworkBehaviour
 {
+
+    public float movementSpeed = 0.5f;
     void HandleMovement()
     {
         if(isLocalPlayer)
         {
             float moveHorizontal = Input.GetAxis("Horizontal");
             float moveVertical = Input.GetAxis("Vertical");
-            Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
+            Vector3 movement = new Vector3(moveHorizontal * movementSpeed, moveVertical * movementSpeed, 0);
             transform.position = transform.position + movement;
         }
     }
 
     private void Update()
     {
+        //I have added a comment
         HandleMovement();
     }
 }
